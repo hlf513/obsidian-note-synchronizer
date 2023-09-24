7b754157f2b4f264909f9499899a71cb90eff353
hlf513 <hlf513@gmail.com>
2023-09-24 21:18:18 +0800

    revert main.ts

---
+++ main.ts
@@ -0,0 +1,184 @@
+import { normalizePath, Notice, Plugin } from 'obsidian';
+import Anki, { AnkiError } from 'src/anki';
+import locale from 'src/lang';
+import { MediaManager } from 'src/media';
+import Note, { NoteManager } from 'src/note';
+import AnkiSynchronizerSettingTab, { DEFAULT_SETTINGS, Settings } from 'src/setting';
+import { NoteDigest, NoteState, NoteTypeDigest, NoteTypeState } from 'src/state';
+import { version } from './package.json';
+
+interface Data {
+  version: string;
+  settings: Settings;
+  noteState: Record<string, NoteDigest>;
+  noteTypeState: Record<string, NoteTypeDigest>;
+}
+
+export default class AnkiSynchronizer extends Plugin {
+  anki = new Anki();
+  settings = DEFAULT_SETTINGS;
+  mediaManager = new MediaManager();
+  noteManager = new NoteManager(this.settings);
+  noteState = new NoteState(this);
+  noteTypeState = new NoteTypeState(this);
+
+  async onload() {
+    // Recover data from local file
+    const data: Data | null = await this.loadData();
+    if (data) {
+      const { settings, noteState, noteTypeState } = data;
+      Object.assign(this.settings, settings);
+      for (const key in noteState) {
+        this.noteState.set(parseInt(key), noteState[key]);
+      }
+      for (const key in noteTypeState) {
+        this.noteTypeState.set(parseInt(key), noteTypeState[key]);
+      }
+    }
+    this.configureUI();
+    console.log(locale.onLoad);
+  }
+
+  configureUI() {
+    // Add import note types command
+    this.addCommand({
+      id: 'import',
+      name: locale.importCommandName,
+      callback: async () => await this.importNoteTypes()
+    });
+    this.addRibbonIcon('enter', locale.importCommandName, async () => await this.importNoteTypes());
+
+    // Add synchronize command
+    this.addCommand({
+      id: 'synchronize',
+      name: locale.synchronizeCommandName,
+      callback: async () => await this.synchronize()
+    });
+    this.addRibbonIcon(
+      'sheets-in-box',
+      locale.synchronizeCommandName,
+      async () => await this.synchronize()
+    );
+
+    // Add a setting tab to configure settings
+    this.addSettingTab(new AnkiSynchronizerSettingTab(this.app, this));
+  }
+
+  // Save data to local file
+  save() {
+    return this.saveData({
+      version: version,
+      settings: this.settings,
+      noteState: Object.fromEntries(this.noteState),
+      noteTypeState: Object.fromEntries(this.noteTypeState)
+    });
+  }
+
+  async onunload() {
+    await this.save();
+    console.log(locale.onUnload);
+  }
+
+  // Retrieve template information from Obsidian core plugin "Templates"
+  getTemplatePath() {
+    const templatesPlugin = (this.app as any).internalPlugins?.plugins['templates'];
+    if (!templatesPlugin?.enabled) {
+      new Notice(locale.templatesNotEnabledNotice);
+      return;
+    }
+    if (templatesPlugin.instance.options.folder === undefined) {
+      new Notice(locale.templatesFolderUndefinedNotice);
+      return;
+    }
+    return normalizePath(templatesPlugin.instance.options.folder);
+  }
+
+  async importNoteTypes() {
+    new Notice(locale.importStartNotice);
+    const templatesPath = this.getTemplatePath();
+    if (templatesPath === undefined) return;
+    this.noteTypeState.setTemplatePath(templatesPath);
+    const noteTypesAndIds = await this.anki.noteTypesAndIds();
+    if (noteTypesAndIds instanceof AnkiError) {
+      new Notice(locale.importFailureNotice);
+      return;
+    }
+    const noteTypes = Object.keys(noteTypesAndIds);
+    const noteTypeFields = await this.anki.multi<{ modelName: string }, string[]>(
+      'modelFieldNames',
+      noteTypes.map(s => ({ modelName: s }))
+    );
+    if (noteTypeFields instanceof AnkiError) {
+      new Notice(locale.importFailureNotice);
+      return;
+    }
+    const state = new Map<number, NoteTypeDigest>(
+      noteTypes.map((name, index) => [
+        noteTypesAndIds[name],
+        {
+          name: name,
+          fieldNames: noteTypeFields[index]
+        }
+      ])
+    );
+    console.log(`Retrieved note type data from Anki`, state);
+    await this.noteTypeState.change(state);
+    await this.save();
+    new Notice(locale.importSuccessNotice);
+  }
+
+  async synchronize() {
+    const templatesPath = this.getTemplatePath();
+    if (templatesPath === undefined) return;
+    new Notice(locale.synchronizeStartNotice);
+    const allFiles = this.app.vault.getMarkdownFiles();
+    const state = new Map<number, [NoteDigest, Note]>();
+    for (const file of allFiles) {
+      // ignore templates
+      if (file.path.startsWith(templatesPath)) continue;
+
+      // read and validate content
+      const frontmatter = this.app.metadataCache.getFileCache(file)?.frontmatter;
+      if (!frontmatter) continue;
+
+      const content = await this.app.vault.read(file);
+      const media = this.app.metadataCache.getFileCache(file)?.embeds;
+
+      const [note, mediaNameMap] = this.noteManager.validateNote(
+        file,
+        frontmatter,
+        content,
+        media,
+        this.noteTypeState
+      );
+      if (!note) continue;
+      if (note.nid === 0) {
+        // new file
+        const nid = await this.noteState.handleAddNote(note, file);
+        if (nid === undefined) {
+          new Notice(locale.synchronizeAddNoteFailureNotice(file.basename));
+          continue;
+        }
+        note.nid = nid;
+        this.app.vault.modify(file, this.noteManager.dump(note, mediaNameMap));
+      }
+      state.set(note.nid, [note.digest(), note]);
+      if (this.noteState.get(note.nid)?.hash !== note.digest().hash) {
+        console.log('update media for note ' + note.basename);
+        if (media) {
+          for (const item of media) {
+            if (!item.link.includes('.') || item.link.includes('.canvas')) {
+              continue;
+            }
+            this.noteState.handleAddMedia(
+              this.mediaManager.parseMedia(item, this.app.vault, this.app.metadataCache)
+            );
+          }
+        }
+      }
+    }
+    await this.noteState.change(state);
+    await this.save();
+    new Notice(locale.synchronizeSuccessNotice);
+  }
+}
